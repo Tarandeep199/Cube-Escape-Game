f02ee13bbb2e20bb5fa483efd03e946c38e77f9d
Tarandeep <tarandeepsingh199@gmail.com>
2019-04-30 22:21:30 -0700

    Add files via upload

---
+++ PlayerCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public movement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+
+
+
+        }
+        
+    }
+}
---
+++ movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    public float upwardsSpace = 0.000001f;
+
+
+
+
+
+    // Start is caled before the first frame update
+
+    void Start()
+    {
+        Debug.Log("okkk");
+
+     
+
+    }
+
+
+    // Update is called once per frame
+    //Use FixedUpdate vs just Update when dealing with physics aspects
+    
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);   //adds a forward force
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, upwardsSpace * Time.deltaTime,0, ForceMode.VelocityChange);
+        }
+
+
+
+    }
+}
